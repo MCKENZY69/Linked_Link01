b8abd55762967543065eaa94a6769daf515eaa2e
jhonedersanmguel <jhonedersanmiguel@gmail.com>
2022-04-21 14:38:32 -0500

    eche

--- Wyandel.py
+++ Wyandel.py
@@ -1,1 +1,12 @@
-print("Hello World")
+print("holi :D, digita el numero 1")
+x = int(input())
+y = 0
+while y != 10000:
+    print("digite el numero 1")
+    if x == 1:
+        print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        print("mira el video bro, tkm")
+        break
+    elif x != 1:
+        y += 1
+        
